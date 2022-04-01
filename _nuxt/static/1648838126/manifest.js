a15cc76b88bcf68264c516749c5870f97e409516
rayuniday <97986074+rayuniday@users.noreply.github.com>
2022-04-02 03:53:38 +0900

    bars css로 변경

--- _nuxt/static/1648838126/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnuxt-content","\u002Fexpress-setup","\u002Fnuxt-basic","\u002F1-book-220401-twenty-ninth-birthday-decided","\u002Fnuxt-highlightjs","\u002Fscraping-request-bs4","\u002Fnative-expo-cli","\u002Fnuxt-sass","\u002Fvue-220401-bottom-top","\u002Fvue-cli","\u002Fnuxt-font-awesome","\u002Freact-basic"]})
---
+++ _nuxt/static/1648839175/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnative-expo-cli","\u002Fnuxt-basic","\u002Fexpress-setup","\u002Fnuxt-font-awesome","\u002Fnuxt-highlightjs","\u002Fnuxt-content","\u002Fvue-cli","\u002Fnuxt-sass","\u002F1-book-220401-twenty-ninth-birthday-decided","\u002Fvue-220401-bottom-top","\u002Freact-basic","\u002Fscraping-request-bs4"]})
