2134729e2cb89731d89b9c9235e06055ebbe1cde
rayuniday <97986074+rayuniday@users.noreply.github.com>
2022-04-02 03:09:55 +0900

    highlightjs 직접 추가

--- _nuxt/static/1648835930/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnative-expo-cli","\u002Fexpress-setup","\u002Fnuxt-basic","\u002Fnuxt-font-awesome","\u002Fnuxt-highlightjs","\u002Fnuxt-sass","\u002Fscraping-request-bs4","\u002Freact-basic","\u002Fvue-220401-bottom-top","\u002F1-book-220401-twenty-ninth-birthday-decided","\u002Fnuxt-content","\u002Fvue-cli"]})
--- _nuxt/static/1648835930/vue-220401-bottom-top/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648835930",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvue-220401-bottom-top",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"688c47fa"}}}}(null));
